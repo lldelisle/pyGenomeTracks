7ea96a964d03b11c05dd92bf05faf014c796c205
Lucille Delisle <lucille.delisle@epfl.ch>
2019-10-01 17:33:55 +0200

    linting

--- pygenometracks/plotters.py
+++ pygenometracks/plotters.py
@@ -1,19 +1,19 @@
-import numpy as np                    
+import numpy as np
 from matplotlib.patches import Polygon
 from matplotlib.patches import Rectangle
-def draw_gene_simple(ax, bed, ypos, patch_height = None, arrow_length= None,
-                     rgb=None, edgecolor=None, linewidth=None, 
+
+
+def draw_gene_simple(ax, bed, ypos, patch_height=None, arrow_length=None,
+                     rgb=None, edgecolor=None, linewidth=None,
                      **kwargs
-                    ):
+                     ):
     """
     draws an interval with direction (if given)
     """
     if arrow_length is None:
-        arrow_length = np.diff(ax.get_xlim())[0]/20.
+        arrow_length = np.diff(ax.get_xlim())[0] / 20.
     if patch_height is None:
-        patch_height = np.diff(ax.get_ylim())[0]/5.
-        
-    from matplotlib.patches import Polygon
+        patch_height = np.diff(ax.get_ylim())[0] / 5.
 
     if bed.strand not in ['+', '-']:
         ax.add_patch(Rectangle((bed.start, ypos), bed.end - bed.start, patch_height,
@@ -21,13 +21,14 @@ def draw_gene_simple(ax, bed, ypos, patch_height = None, arrow_length= None,
                                **kwargs))
     else:
 
-        vertices = _draw_arrow(bed.start, bed.end, bed.strand, ypos, patch_height, arrow_length )
+        vertices = _draw_arrow(bed.start, bed.end, bed.strand, ypos, patch_height, arrow_length)
         ax.add_patch(Polygon(vertices, closed=True, fill=True,
                              edgecolor=edgecolor,
                              facecolor=rgb,
                              linewidth=linewidth,
                              **kwargs))
 #     _draw_arrow
+
 
 def _draw_arrow(start, end, strand, ypos, height, arrow_length):
     """
@@ -49,7 +50,7 @@ def _draw_arrow(start, end, strand, ypos, height, arrow_length):
         starting in the lower left corner and progressing in a clock wise manner.
 
         -----------------\
-        ---------------- /
+        -----------------/
 
         """
 
@@ -64,10 +65,9 @@ def _draw_arrow(start, end, strand, ypos, height, arrow_length):
         The vertices correspond to 5 points along the path of a form like the following,
         starting in the lower left corner and progressing in a clock wise manner.
 
-        /-----------------
-        \-----------------
+        / -----------------
+        \\-----------------
         """
         vertices = [(x0, y0), (x0 - arrow_length, y0 + half_height), (x0, y1), (x1, y1), (x1, y0)]
 
     return vertices
-
