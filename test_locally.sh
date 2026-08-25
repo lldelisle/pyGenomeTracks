114c55c44b180cd708f361cec1e5101575c342b9
Lucille Delisle <lucille.delisle@unige.ch>
2026-08-25 22:07:02 +0200

    add new tests for last python versions

--- test_locally.sh
+++ test_locally.sh
@@ -22,11 +22,19 @@ coverage html
 coverage-badge -f -o docs/coverage.svg
 micromamba deactivate
 
-# TRAVIS_PYTHON_VERSION=3.11
-# conda create -n pgt_test_${TRAVIS_PYTHON_VERSION} --yes -c bioconda -c conda-forge python=$TRAVIS_PYTHON_VERSION
-# conda activate pgt_test_${TRAVIS_PYTHON_VERSION}
-# conda install --yes -c conda-forge -c bioconda bedtools
-# pip install -r requirements_CI.txt
-# python setup.py install
-# py.test pygenometracks --doctest-modules
-# conda deactivate
+
+TRAVIS_PYTHON_VERSION=3.14
+for dir in $(find . -name "__pycache__"); do
+    rm -r $dir
+done
+if [ -e build ]; then
+    rm -r build
+fi
+micromamba create -n pgt_test_${TRAVIS_PYTHON_VERSION} --yes -c bioconda python=$TRAVIS_PYTHON_VERSION
+micromamba activate pgt_test_${TRAVIS_PYTHON_VERSION}
+micromamba install --yes -c bioconda bedtools
+requirement_file=requirements_CI.txt
+python -m pip install -r ${requirement_file}
+python -m pip install .
+py.test pygenometracks --doctest-modules
+micromamba deactivate
